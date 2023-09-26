7e05c6ba6dec9e81b46f5184d147f2edb56d6141
SempaiOchilov <139633008+SempaiOchilov@users.noreply.github.com>
2023-09-26 14:51:10 +0500

    Add files via upload

--- scriptdz.js
+++ scriptdz.js
@@ -27,153 +27,216 @@
 //if( c.length % 2 === 0) {
   //  alert ('welcome')
 //}
-//else
-//{
-  //  alert ('иди в садик')
-//}
+// //else
+// //{
+//   //  alert ('иди в садик')
+// //}
 
 
 
 
-//...........................................................................................................................................
-
-
-
-
-
-
-// let name =prompt("как вас зовут?")
-// let account = "7777"
-// let money ="10000"
-
-// if(name === "Alex") {
-//   +prompt("number schet")
-//    if (name === 7777){
-//     alert("doog")
-//   }
-// }
+// //...........................................................................................................................................
 
 
 
 
 
 
+// // let name =prompt("как вас зовут?")
+// // let account = "7777"
+// // let money ="10000"
 
-//.......................................................................................................................................
-
-
-
-
-
-// let guest =+prompt("сколько занято")
-// let name =prompt("как вас зовут")
-// let age =+prompt("сколько вам лет")
-// let money =+prompt("сколько у вас денег")
-// if(name.charAt(0).toLowerCase() === "a"&& age >= 20 && age <= 40 && money>=100){
-//   alert("welcome")
-// }
-// else{
-//   alert("good bye")
-// }
-
-// let guest=+prompt("cколько людей в клубе")
-// let name=prompt("как вас зовут")
+// // if(name === "Alex") {
+// //   +prompt("number schet")
+// //    if (name === 7777){
+// //     alert("doog")
+// //   }
+// // }
 
 
 
-// if(name.charAt(0).toLowerCase() === "a"){
 
-//    let age =+prompt("сколько вам лет")
 
-//      if( age >= 20 && age <= 40){
 
-//        let money =+prompt("сколько у вас денег")
 
-//          if(money >= 100){
+// //.......................................................................................................................................
+
+
+
+
+
+// // let guest =+prompt("сколько занято")
+// // let name =prompt("как вас зовут")
+// // let age =+prompt("сколько вам лет")
+// // let money =+prompt("сколько у вас денег")
+// // if(name.charAt(0).toLowerCase() === "a"&& age >= 20 && age <= 40 && money>=100){
+// //   alert("welcome")
+// // }
+// // else{
+// //   alert("good bye")
+// // }
+
+// // let guest=+prompt("cколько людей в клубе")
+// // let name=prompt("как вас зовут")
+
+
+
+// // if(name.charAt(0).toLowerCase() === "a"){
+
+// //    let age =+prompt("сколько вам лет")
+
+// //      if( age >= 20 && age <= 40){
+
+// //        let money =+prompt("сколько у вас денег")
+
+// //          if(money >= 100){
 
        
-//           let guestlist =+prompt("сколько вас")
+// //           let guestlist =+prompt("сколько вас")
 
-//              if(guest + guestlist<= 10){
-//                alert("welcome ")
-//              }
+// //              if(guest + guestlist<= 10){
+// //                alert("welcome ")
+// //              }
 
-//               else{
-//                alert("иди в другой клуб нет мест")
-//               }
+// //               else{
+// //                alert("иди в другой клуб нет мест")
+// //               }
            
 
-//            }
-//          else {
-//          alert("good bye")
-//          }
-//     }
-//     else {
-//     alert("good bye")
-//    }
+// //            }
+// //          else {
+// //          alert("good bye")
+// //          }
+// //     }
+// //     else {
+// //     alert("good bye")
+// //    }
 
+// // }
+// // else {
+// //   alert("good bye")
+// // }
+
+
+
+
+// // 11111111111
+// //// let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+
+// // let from = prompt('цена от')
+// // let up = prompt('цена до')
+
+
+// //22222222222
+// // let students = ['baxtzod', 'xamza', 'islom', 'abdumanon', 'xojik', 'aziz'] // longest name
+
+
+
+// let pricelist =[ 4000,2000,15000,23000,35000,50000,97000,40000 ]
+//  let from = +prompt('цена от')
+//  let up = +prompt('цена до')
+
+
+//  if(from >  1000 && up <= 10000){
+//   alert(" Мы можем предлажить вам такие авто как: Tico , Matiz , Nexia , Spark")
+
+//  }
+//  else if(from > 12000 && up <= 30000){
+//   alert(" Мы можем предлажить вам такие авто как: Traker 2, Jentra, BYD ")
 // }
-// else {
-//   alert("good bye")
+
+// else if(from > 35000 && up <= 70000){
+//   alert(" Мы можем предлажить вам такие авто как: Camaro, Corvette , Tesla, Cherry ")
+// }
+
+// else if(from > 70000 && up <= 100000){
+//   alert(" Мы можем предлажить вам такие авто как: Taxo, Tesla X ")
 // }
 
 
 
 
-// 11111111111
-//// let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+//  else {
+//   alert("good bye bitch")
+//  }
 
-// let from = prompt('цена от')
-// let up = prompt('цена до')
+//  ///////////////////////////////////////////////////////////////////////////////////////////////////
+//  let students = [
+//   { name: "Safir" },
+//   { name: "Baxtzod" },
+//   { name: "Islom" },
+//   { name: "eva_elfi" },
+//   { name: "Shaxzod" },
+// ];
+
+// let longestname = students.filter((student) => {
+
+//   return student.name.length === Math.max(...students.map(({ name }) => name.length));
+// })[0].name;
+
+// alert(`Самое длинное имя: ${longestname}`);
 
 
-//22222222222
-// let students = ['baxtzod', 'xamza', 'islom', 'abdumanon', 'xojik', 'aziz'] // longest name
 
 
 
-let pricelist =[ 4000,2000,15000,23000,35000,50000,97000,40000 ]
- let from = +prompt('цена от')
- let up = +prompt('цена до')
+// JS Homework 5
+
+// Тест на нахождение ключей
+// Сложить все цифры из массива с объектами в переменную `total`
+
+let arr = [
+  [{a: {price: 21}}],
+  [{a: {price: 35}}],
+  [{a: {price: 44}}]
+]
 
 
- if(from >  1000 && up <= 10000){
-  alert(" Мы можем предлажить вам такие авто как: Tico , Matiz , Nexia , Spark")
 
- }
- else if(from > 12000 && up <= 30000){
-  alert(" Мы можем предлажить вам такие авто как: Traker 2, Jentra, BYD ")
+let total = 0;
+for (let c = 0; c < arr.length; c++) {
+  for (let key in arr[c][0]) {
+    total += arr[c][0][key].price;
+  }
+}
+console.log(total);
+
+
+
+
+
+
+let arrr = [4, 16, 19, 22, 11, 144, 967, 19124];
+let even = [];
+let odd = [];
+
+for (let c = 0; c < arrr.length; c++) {
+  if (arrr[c] % 2 === 0) {
+    even.push(arrr[c]);
+  } else {
+    odd.push(arrr[c]);
+  }
 }
 
-else if(from > 35000 && up <= 70000){
-  alert(" Мы можем предлажить вам такие авто как: Camaro, Corvette , Tesla, Cherry ")
-}
-
-else if(from > 70000 && up <= 100000){
-  alert(" Мы можем предлажить вам такие авто как: Taxo, Tesla X ")
-}
+console.log(even); 
+console.log(odd); 
 
 
 
 
- else {
-  alert("good bye bitch")
- }
 
- ///////////////////////////////////////////////////////////////////////////////////////////////////
- let students = [
-  { name: "Safir" },
-  { name: "Baxtzod" },
-  { name: "Islom" },
-  { name: "eva_elfi" },
-  { name: "Shaxzod" },
-];
 
-let longestname = students.filter((student) => {
 
-  return student.name.length === Math.max(...students.map(({ name }) => name.length));
-})[0].name;
 
-alert(`Самое длинное имя: ${longestname}`);
+
+
+
+
+
+
+
+
+
+
 
 
